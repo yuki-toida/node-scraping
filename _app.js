4a8e608f5f6bfea047aacbe28f383cd32f1e2200
YukiToida <y.toida@gloops.com>
2014-09-09 22:01:30 +0900

    exclude async

--- _app.js
+++ _app.js
@@ -1,26 +1,13 @@
 var client = require('cheerio-httpcli');
-var async = require('async');
 
 var titles = [];
 var urls = [];
 
-async.series([
-    function(callback) {
-        client.fetch('https://www.youtube.com/results', { search_query: 'premier+league' }, function(err, $, res) {
-            $("ol[class='item-section']").children("li").each(function(idx) {
-                var target = $(this).find("h3[class='yt-lockup-title']").children("a");
-                titles[idx] = target.attr("title");
-                urls[idx] = target.attr("href");
-            });
-            console.log(titles.length);
-        })
-    },
-    function(callback) {
-        console.log(titles.length);
-    }
-], function (err, results) {
-    if (err) {
-        throw err;
-    }
-    console.log('last');
+client.fetch('https://www.youtube.com/results', { search_query: 'premier+league' }, function (err, $, res) {
+    $("ol[class='item-section']").children("li").each(function (idx) {
+        var target = $(this).find("h3[class='yt-lockup-title']").children("a");
+        titles[idx] = target.attr("title");
+        urls[idx] = target.attr("href");
+        console.log(titles[idx]);
+    });
 });
